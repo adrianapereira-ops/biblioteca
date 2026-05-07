80a21c59817c02b0f26e6261620f128de9012df4
alexia164 <alexapirau@gmail.com>
2026-05-07 20:34:27 +0200

    moddified gestor biblioteca

--- GestorBiblioteca.java
+++ GestorBiblioteca.java
@@ -49,7 +49,7 @@ public class GestorBiblioteca {
             }
 
             mostrarLlibreMesPrestat();
-            mostrarUsuariMesActiu();
+            
         }
 
         private void mostrarLlibreMesPrestat() {
