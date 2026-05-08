0f657bebc0f6cb5d8c84d0a165a1383b1b6ccda9
Adriana <adriana.pereira@insbaixcamp.cat>
2026-05-08 13:18:05 +0200

    llibre editat

--- Libre.java
+++ Libre.java
@@ -51,3 +51,5 @@ public class Llibre {
         return titol + " de " + autor + 
                " [" + categoria + "]" +          
                (prestat ? " (En préstec)" : " (Disponible)");
+
+    }
